80b8fb7799deafff3e1d62d35b060aa43dd77ec9
societyoutsiders <societyoutsiders@gmail.com>
2025-09-07 01:25:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/test3/' // <-- Diese Zeile hinzufügen
+  base: '/test3/' // <-- Diese Zeile muss so aussehen
 })
